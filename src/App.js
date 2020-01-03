50e20310bbbc7f8356e9c1c7f0f5bdf52458acbb
kevin <tenngawang6080@gmail.com>
2020-01-02 23:30:26 -0500

    updated with real question and answer

--- src/App.js
+++ src/App.js
@@ -10,15 +10,42 @@ class App extends React.Component{
   componentDidMount = () => {
     this.props.setQuestions([
       {
-        question: "What is JS stand for?",
-        answer: "JavaScript",
-        options: ["JavaScript" , "JustScript", "JavaSystem", "JuniorScript" ]
+        question: "Inside which HTML element do we put the JavaScript?",
+        answer: "<script>",
+        options: ["<script>" , "<javascript>", "<scripting>", "<js>" ]
     },
     {
-        question: "What is first language?",
+        question: `What is the correct JavaScript syntax to change the content of the HTML element here => <p id="geek">GeeksforGeeks</p> `,
         answer: "JavaScript",
-        options: ["JavaScript" , "JustScript", "JavaSystem", "JuniorScript" ]
-    }
+        options: [
+          "document.getElement(“geek”).innerHTML=”I am a Geek”;" ,
+          "document.getElementById(“geek”).innerHTML=”I am a Geek”;",
+          "document.getId(“geek”)=”I am a Geek”;", 
+          "document.getElementById(“geek”).innerHTML=I am a Geek;" ]
+    },
+    {
+      question: "Which of the following is the correct syntax to display “GeeksforGeeks” in an alert box using JavaScript?",
+      answer: "alert(“GeeksforGeeks”);",
+      options: [
+        "alertbox(“GeeksforGeeks”);" , 
+        "msg(“GeeksforGeeks”);", 
+        "msgbox(“GeeksforGeeks”);", 
+        "alert(“GeeksforGeeks”);" ]
+  },
+  {
+    question: "What is the correct syntax for referring to an external script called “geek.js”?",
+    answer: "<script src=”geek.js”>",
+    options: [
+      "<script src=”geek.js”>" , 
+    "<script href=”geek.js”>", 
+    "<script ref=”geek.js”>", 
+    "<script name=”geek.js”>" ]
+},
+{
+  question: "The external JavaScript file must contain <script> tag. True or False?",
+  answer: "False",
+  options: ["True" , "False", "None", "Both" ]
+},
     ]);
   }
   
